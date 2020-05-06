4c943b77319b8a76818f7e950afcfc3389873e54
Ronmel Ross <ronmel_ross@ligph.com>
2020-05-06 23:28:32 +0800

    [#44] Send HTML Emails Using Mailable Classes

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\ContactMe;
 use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
@@ -18,12 +19,15 @@ class ContactController extends Controller
         //$email = request('email');
         //dd($email);
 
-        Mail::raw('It works!', function ($message) {
-            $message->from('john@johndoe.com', 'John Doe');
-            $message->sender('john@johndoe.com', 'John Doe');
-            $message->to(request('email'));
-            $message->subject('Subject');
-        });
+        // Mail::raw('It works!', function ($message) {
+        //     $message->from('john@johndoe.com', 'John Doe');
+        //     $message->sender('john@johndoe.com', 'John Doe');
+        //     $message->to(request('email'));
+        //     $message->subject('Subject');
+        // });
+
+        Mail::to(request('email'))
+            ->send(new ContactMe('Football'));
 
         return redirect('/contact')
             ->with('message','Email Sent!');
---
+++ resources/views/emails/contact-me.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>It works</h1>
+    <div>
+        <h2>About {{ $topic }}</h2>
+        Visit us at <a href="#">www.{{ $topic }}.com</a>
+    </div>
+    
+</body>
+</html>
