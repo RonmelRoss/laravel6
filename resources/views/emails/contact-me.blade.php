1e4c624efab693975d170da149e3e2e9f48592c0
Ronmel Ross <ronmel_ross@ligph.com>
2020-05-07 23:06:29 +0800

    [#45] Send Email Using Markdown Templates

--- resources/views/emails/contact-me.blade.php
+++ resources/views/emails/contact-me.blade.php
@@ -1,17 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>It works</h1>
-    <div>
-        <h2>About {{ $topic }}</h2>
-        Visit us at <a href="#">www.{{ $topic }}.com</a>
-    </div>
-    
-</body>
-</html>
+@component('mail::message')
+
+# A Heading
+
+Lorem ipsum dolor sit amet.
+
+- A list
+- of differnt
+- TODOs
+
+@component('mail::button', ['url' => 'https://laracast.com'])
+    Visit Laracasts
+@endcomponent
+
+@endcomponent
